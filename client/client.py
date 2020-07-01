ea2a1785fd372c2ffc2cf7ef4f69c59c0fad69e1
Joe Reid <josephreid1991@gmail.com>
2020-07-01 21:48:16 +0100

    added more functionality to the client end of the application

--- client/client.py
+++ client/client.py
@@ -8,10 +8,12 @@ and sending a report to a given server
 
 
 import psutil
+import socket
 
 class ClientReport:
-    def __init__(self):
-        pass
+    def __init__(self, ipAddress, port):
+        self.address = ipAddress
+        self.port = port
     
     def generateReport(self):
         #get running processes on the system
@@ -24,4 +26,12 @@ class ClientReport:
         
         return reportString
     
+    def sendReport(self):
+        report = self.generateReport()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.address,self.port))
+        self.socket.send(bytes(report, "utf-8"))
+        self.socket.close()
+        
+        
     
--- client/clienttest.py
+++ client/clienttest.py
@@ -7,19 +7,48 @@ Unit tests for the client object
 
 
 import unittest
+import socket
 from client import ClientReport
 
 class TestClient(unittest.TestCase):
     
     def setUp(self):
-        self.testReporter = ClientReport()
+        self.localhost = '127.0.0.1'
+        self.port = 55556
+        self.testReporter = ClientReport(self.localhost, self.port)
+        self.testSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.testSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.testSocket.bind((self.localhost, self.port))
     
+    def tearDown(self):
+        self.testSocket.close()
+        
     def testProcessListGeneration(self):
         self.assertIsNotNone(self.testReporter.generateReport(), "Nothing is generated")
         
     def testProcessListReadable(self):
         self.assertTrue(type(self.testReporter.generateReport()) == str, "The generated report is not a string")
 
+    def testConnectionCapability(self):
+        self.testSocket.settimeout(1)
+        self.testSocket.listen()
+        self.testReporter.sendReport()
+        while True:
+            clientsocket, address = self.testSocket.accept()
+            self.assertTrue(1,1)
+            break
+        
+    def testMessageContainsInfo(self):
+        self.testSocket.settimeout(1)
+        self.testSocket.listen()
+        self.testReporter.sendReport()
+        while True:
+            clientsocket, address = self.testSocket.accept()
+            data = clientsocket.recv(1000000)
+            data = data.decode("utf-8")
+            self.assertIn('pid', data)
+            self.assertIn('Name', data)
+            break
     
 if __name__ == '__main__':
     unittest.main()
