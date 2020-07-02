044198e82ad03d86fa3f5b23694d571addd3f302
Joe Reid <josephreid1991@gmail.com>
2020-07-02 17:38:40 +0100

    added example running script for client side

---
+++ client/clientRun.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  2 17:37:05 2020
+
+@author: josep
+"""
+
+from client import ProgramReporter
+
+reporter = ProgramReporter('87.254.4.248', 55555)
+reporter.report()
